279010e94c125e09122064cd222004113cabae6b
kishor darkunde <kishordarkunde4@gmail.com>
2025-02-04 11:40:37 +0530

    email works at contact me

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -13,7 +13,7 @@ export const Contact = () => {
       <ul className={styles.links}>
         <li className={styles.link}>
           <img src={getImageUrl("contact/emailIcon.png")} alt="Email icon" />
-          <a href="kishordarkunde4@gmail.com">kishordarkunde4@gmail.com</a>
+          <a href="mailto:kishordarkunde4@gmail.com">kishordarkunde4@gmail.com</a>
         </li>
         <li className={styles.link}>
           <img
